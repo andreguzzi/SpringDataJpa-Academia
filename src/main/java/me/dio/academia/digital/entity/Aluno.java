b9b8068d0cc4efe2ba427b7d91ef3bba96cfcc81
andreguzzi <andre_guzzi@hotmail.com>
2023-03-08 23:28:28 -0300

    first commit

---
+++ src/main/java/me/dio/academia/digital/entity/Aluno.java
@@ -0,0 +1,45 @@
+package me.dio.academia.digital.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+//Data do lombok faz o get set e hashcode
+//NoArgsConstructor e AllArgsConstrutor, tem 2 construtores um vazio e outro constando os atributos
+//Entity, toda entidade tem um Id
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Aluno {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+
+  @Column(unique = true)
+  private String cpf;
+
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+
+  //um aluno tem varias avaliações fisicas
+  //lazy preguiçoso só vai carregar se chamar
+  //JsonIgnore é usado para ignorar exceptions que ocorre quando se usa lazy
+  @OneToMany(mappedBy = "aluno", cascade = CascadeType.REMOVE , fetch = FetchType.LAZY)
+  @JsonIgnore
+  private List<AvaliacaoFisica> avaliacoes = new ArrayList<>();
+
+}
